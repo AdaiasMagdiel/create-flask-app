191067e958e81f905fa35310fd955164c6186fc9
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-01-09 17:50:33 -0300

    feat: add traceback in error handler

--- template/app/routes/error_handler.py
+++ template/app/routes/error_handler.py
@@ -1,17 +1,15 @@
 from flask import Flask
+import traceback
 
 
 class ErrorHandler:
     def __init__(self, app: Flask) -> None:
-        app.register_error_handler(400, self.error_400)
-        app.register_error_handler(404, self.error_404)
-        app.register_error_handler(422, self.error_422)
+        app.register_error_handler(400, self.error)
+        app.register_error_handler(404, self.error)
+        app.register_error_handler(422, self.error)
 
-    def error_400(self, e):
-        return {'error': e.description}, 400
+    def error(self, e):
+        traceback.print_exc()
+        code = e.code if hasattr(e, 'code') else 500
 
-    def error_404(self, e):
-        return {'error': 'The current endpoint was not found'}, 404
-
-    def error_422(self, e):
-        return {'error': e.description}, 422
+        return {'error': e.description, 'code': code}, code
