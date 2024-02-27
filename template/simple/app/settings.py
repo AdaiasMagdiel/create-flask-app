6a9b803a6296c1d5f8d61f77c310e1460996b12f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-02-26 21:36:49 -0300

    refactor(settings): Enhance environment variable management
    Improve the management of environment variables in the settings.py file.
    - Previously, the dotenv library was imported but not used correctly.
    - Now, the load_dotenv function is called to load the environment variables.
    - Environment variables are now loaded from os.environ instead of dotenv_values().
    - New environment variables ENV_MODE, DETA_KEY, and CACHE_TABLE are defined.

--- template/simple/app/settings.py
+++ template/simple/app/settings.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
 
 class Config:
+    ENV_MODE: str
+    DETA_KEY: str
+    CACHE_TABLE: str
+
     def init_app(self, app: Flask) -> None:
-        for key, value in dotenv_values().items():
+        load_dotenv()
+
+        for key, value in os.environ.items():
             app.config[key] = value
             setattr(self, key, value)
 
