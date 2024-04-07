83c473bfb7074d7dfc928934fe555716297ac352
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-07 19:02:28 -0300

    feat(app-config): update variable name in settings.py
    - Renamed `DETA_KEY` to `DETA_PROJECT_KEY` for consistency.
    - Added a method `__set_flask_configs` to handle setting Flask configs based on environment variables prefixed with `FLASK_`.

--- template/simple/app/settings.py
+++ template/simple/app/settings.py
@@ -5,15 +5,20 @@ from dotenv import load_dotenv
 
 class Config:
     ENV_MODE: str
-    DETA_KEY: str
+    DETA_PROJECT_KEY: str
     CACHE_TABLE: str
 
     def init_app(self, app: Flask) -> None:
         load_dotenv()
 
         for key, value in os.environ.items():
-            app.config[key] = value
+            self.__set_flask_configs(app, key, value)
             setattr(self, key, value)
+
+    def __set_flask_configs(self, app: Flask, key: str, value: str) -> None:
+        if 'FLASK_' in key.upper():
+            key = key.replace('FLASK_', '')
+            app.config[key] = value
 
 
 settings = Config()
